a4efa485b1dbc4045c588fa44d12c43f6370b874
Breyner <breyimp@gmail.com>
2025-05-19 01:51:23 -0500

    file

---
+++ DATABASE_FOREIGNKEY.sql
@@ -0,0 +1,14 @@
+GCOMPEDH:
+    SELECT * FROM gcompedd;
+    SELECT * FROM gproveed;
+    SELECT * FROM cempresa;
+    SELECT * FROM gdelegac;
+    SELECT * FROM gdeparta;
+    SELECT * FROM cterdire;
+    SELECT * FROM galmacen;
+    SELECT * FROM ctiponac;
+    SELECT * FROM cpar_parpreh;
+    SELECT * FROM cper_empleado;
+    SELECT * FROM cterdire;
+    SELECT * FROM cprovinc;
+
